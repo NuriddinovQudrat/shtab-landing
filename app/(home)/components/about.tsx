fc4c1d738684f706d99ef8de46ea5902df37d0d8
NuriddinovQudrat <qudratjonnuriddinov2603@gmail.com>
2024-11-18 10:05:15 +0500

    carousel

--- app/(home)/components/about.tsx
+++ app/(home)/components/about.tsx
@@ -1,19 +1,32 @@
+"use client";
+
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
+import Image from "next/legacy/image";
+
 export const About = () => {
   return (
     <div className="bg-primary p-4 py-10" id="about">
       <div className="container mx-auto">
-        <div className="flex md:flex-row flex-col mb-10">
-          <div className="md:w-1/3 w-full mb-10">
-            <h1 className="flex items-center text-primary-foreground uppercase">
-              <div className="bg-primary-foreground w-5 h-[1px] mr-2"></div>
-              Biz haqimizda
-            </h1>
-          </div>
-          <div className="lg:w-2/3 w-full text-white">
+        <div className="mb-10">
+          <h1 className="flex items-center text-primary-foreground uppercase">
+            <div className="bg-primary-foreground w-5 h-[1px] mr-2"></div>
+            Biz haqimizda
+          </h1>
+        </div>
+
+        <div className="flex items-center justify-between lg:flex-row flex-col-reverse gap-10">
+          <div className="lg:w-1/3 w-full text-white">
             <h1 className="sm:text-4xl text-3xl mb-5">
               Markaziy eksport shtabining asosiy vazifalari:
             </h1>
-
             <div className="flex flex-wrap">
               <div className="flex flex-wrap w-full">
                 <div className="md:py-6">
@@ -96,8 +109,69 @@ export const About = () => {
               </div>
             </div>
           </div>
+
+          <div className="lg:w-2/3 h-full w-full flex items-center justify-center">
+            <Carousel
+              className="w-full"
+              plugins={[
+                Autoplay({
+                  delay: 10000,
+                }),
+              ]}
+            >
+              <CarouselContent>
+                {carouselItems?.map((item, index) => (
+                  <CarouselItem key={index}>
+                    <div className="p-1">
+                      <Card className="bg-transparent border-none flex items-center justify-center w-full">
+                        <CardContent className="flex w-full lg:h-[500px] md:h-[400px] h-[300px] items-center justify-center p-6 relative overflow-hidden">
+                          <Image
+                            src={item.imageUrl}
+                            alt={item.description}
+                            layout="fill"
+                            loading="lazy"
+                            objectFit="cover"
+                            className="rounded-xl"
+                          />
+                        </CardContent>
+                      </Card>
+                    </div>
+                  </CarouselItem>
+                ))}
+              </CarouselContent>
+              <CarouselPrevious />
+              <CarouselNext />
+            </Carousel>
+          </div>
         </div>
       </div>
     </div>
   );
 };
+
+const carouselItems = [
+  {
+    imageUrl: "/carousel/export-shtab1.jpg",
+    description: "export shtab",
+  },
+  {
+    imageUrl: "/carousel/export-shtab2.jpg",
+    description: "export shtab",
+  },
+  {
+    imageUrl: "/carousel/export-shtab3.jpg",
+    description: "export shtab",
+  },
+  {
+    imageUrl: "/carousel/export-shtab4.jpg",
+    description: "export shtab",
+  },
+  {
+    imageUrl: "/carousel/export-shtab5.jpg",
+    description: "export shtab",
+  },
+  {
+    imageUrl: "/carousel/export-shtab6.jpg",
+    description: "export shtab",
+  },
+];
