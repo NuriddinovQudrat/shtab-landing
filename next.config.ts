c96d9ba4c78258692df3fb7a59ce7d970357004e
NuriddinovQudrat <qudratjonnuriddinov2603@gmail.com>
2025-01-10 11:12:28 +0500

    next config configured

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
@@ -8,8 +9,17 @@ const nextConfig: NextConfig = {
         hostname: "dummyimage.com",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "exportstate.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "shtab-landing.vercel.app",
+        pathname: "/**",
+      },
     ],
-    domains: ["localhost", "https://exportstate.com/", "https://shtab-landing.vercel.app/"],
   },
 };
 
