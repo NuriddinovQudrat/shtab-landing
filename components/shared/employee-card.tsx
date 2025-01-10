2376450478af6d5795981b69bf55b2f529029289
NuriddinovQudrat <qudratjonnuriddinov2603@gmail.com>
2025-01-10 11:43:57 +0500

    img fixed

--- components/shared/employee-card.tsx
+++ components/shared/employee-card.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/legacy/image";
-// import { CarouselItem } from "../ui/carousel";
+import { CarouselItem } from "../ui/carousel";
 import { Card, CardContent } from "../ui/card";
 import { cn } from "@/lib/utils";
 
@@ -17,27 +17,27 @@ interface EmployeeCardProps {
 }
 
 const EmployeeCard = ({ employee }: EmployeeCardProps) => {
-  // const [isHover, setIsHover] = useState(false);
+  const [isHover, setIsHover] = useState(false);
 
   return (
-    // <CarouselItem
-    //   className="pl-1 md:basis-1/2 lg:basis-1/3"
-    //   onMouseEnter={() => setIsHover(true)}
-    //   onMouseLeave={() => setIsHover(false)}
-    // >
-    <div className="p-1">
-      <Card>
-        <CardContent className="flex p-0 rounded-md md:h-[500px] h-[400px] items-center justify-center">
-          <div className="w-full h-full rounded-md relative overflow-hidden">
-            <Image
-              src={employee.imgUrl}
-              alt={employee.name}
-              layout="fill"
-              objectPosition={"center"}
-              // className={cn("object-cover transition-all duration-200", isHover && "scale-110")}
-              quality={75}
-            />
-            {/* <div
+    <CarouselItem
+      className="pl-1 md:basis-1/2 lg:basis-1/3"
+      onMouseEnter={() => setIsHover(true)}
+      onMouseLeave={() => setIsHover(false)}
+    >
+      <div className="p-1">
+        <Card>
+          <CardContent className="flex p-0 rounded-md md:h-[500px] h-[400px] items-center justify-center">
+            <div className="w-full h-full rounded-md relative overflow-hidden">
+              <Image
+                src={employee.imgUrl}
+                alt={employee.name}
+                layout="fill"
+                objectPosition={"center"}
+                className={cn("object-cover transition-all duration-200", isHover && "scale-110")}
+                quality={75}
+              />
+              {/* <div
                 className={cn("object-cover transition-all duration-200", isHover && "scale-110")}
                 style={{
                   backgroundImage: `url(${employee.imgUrl})`,
@@ -46,36 +46,36 @@ const EmployeeCard = ({ employee }: EmployeeCardProps) => {
                   backgroundSize: "cover",
                 }}
               ></div> */}
-            <div
-              className={cn(
-                "w-full absolute text-white font-semibold flex flex-col opacity-0 bottom-0 left-0 right-0 h-28 items-center justify-center transition-all duration-200",
-                // isHover && "opacity-100",
-              )}
-              style={{
-                background: "linear-gradient(0deg, #459e5f 0, rgba(0, 0, 0, 0) 100%)",
-              }}
-            >
-              <h1 className="text-2xl">{employee.name}</h1>
-              <p className="text-sm">{employee.position}</p>
-              <div className="flex items-center justify-around text-sm md:flex-row flex-col">
-                <div>
-                  {employee?.tel?.map((tel, index1) => (
-                    <a href={`tel:${tel}`} key={index1}>
-                      {tel}
-                      {index1 !== employee.tel.length - 1 && <span className="mx-1">|</span>}
-                    </a>
-                  ))}
+              <div
+                className={cn(
+                  "w-full absolute text-white font-semibold flex flex-col opacity-0 bottom-0 left-0 right-0 h-28 items-center justify-center transition-all duration-200",
+                  isHover && "opacity-100",
+                )}
+                style={{
+                  background: "linear-gradient(0deg, #459e5f 0, rgba(0, 0, 0, 0) 100%)",
+                }}
+              >
+                <h1 className="text-2xl">{employee.name}</h1>
+                <p className="text-sm">{employee.position}</p>
+                <div className="flex items-center justify-around text-sm md:flex-row flex-col">
+                  <div>
+                    {employee?.tel?.map((tel, index1) => (
+                      <a href={`tel:${tel}`} key={index1}>
+                        {tel}
+                        {index1 !== employee.tel.length - 1 && <span className="mx-1">|</span>}
+                      </a>
+                    ))}
+                  </div>
+                  <a href={`mailto:${employee.email}`} className="ml-4">
+                    {employee.email}
+                  </a>
                 </div>
-                <a href={`mailto:${employee.email}`} className="ml-4">
-                  {employee.email}
-                </a>
               </div>
             </div>
-          </div>
-        </CardContent>
-      </Card>
-    </div>
-    // </CarouselItem>
+          </CardContent>
+        </Card>
+      </div>
+    </CarouselItem>
   );
 };
 
