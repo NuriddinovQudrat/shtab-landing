3d69199edb22d2ea886f659b169089eda9d8b230
NuriddinovQudrat <qudratjonnuriddinov2603@gmail.com>
2025-01-10 12:03:28 +0500

    rasm

--- components/shared/employee-card.tsx
+++ components/shared/employee-card.tsx
@@ -5,6 +5,7 @@ import Image from "next/legacy/image";
 import { CarouselItem } from "../ui/carousel";
 import { Card, CardContent } from "../ui/card";
 import { cn } from "@/lib/utils";
+import Rasm from "@/public/employees/2.jpg";
 
 interface EmployeeCardProps {
   employee: {
@@ -29,23 +30,22 @@ const EmployeeCard = ({ employee }: EmployeeCardProps) => {
         <Card>
           <CardContent className="flex p-0 rounded-md md:h-[500px] h-[400px] items-center justify-center">
             <div className="w-full h-full rounded-md relative overflow-hidden">
-              <Image
+              {/* <Image
                 src={employee.imgUrl}
                 alt={employee.name}
                 layout="fill"
                 objectPosition={"center"}
                 className={cn("object-cover transition-all duration-200", isHover && "scale-110")}
                 quality={75}
-              />
-              {/* <div
+              /> */}
+              <Image
+                src={Rasm.src}
+                alt={employee.name}
+                layout="fill"
+                objectPosition={"center"}
                 className={cn("object-cover transition-all duration-200", isHover && "scale-110")}
-                style={{
-                  backgroundImage: `url(${employee.imgUrl})`,
-                  backgroundRepeat: "no-repeat",
-                  backgroundPosition: "center",
-                  backgroundSize: "cover",
-                }}
-              ></div> */}
+                quality={75}
+              />
               <div
                 className={cn(
                   "w-full absolute text-white font-semibold flex flex-col opacity-0 bottom-0 left-0 right-0 h-28 items-center justify-center transition-all duration-200",
