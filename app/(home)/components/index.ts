cfe94a08dc0e5b8215ac675de3ee6318210e9474
NuriddinovQudrat <qudratjonnuriddinov2603@gmail.com>
2025-01-10 10:58:21 +0500

    import error fixed

--- app/(home)/components/index.ts
+++ app/(home)/components/index.ts
@@ -1,31 +1,5 @@
 import { Navbar } from "./navbar";
 import { Footer } from "./footer";
 import { Hero } from "./hero";
-import { Statistics } from "./statistics";
-import { About } from "./about";
-import { Brands } from "./brands";
-import { Fresh } from "./fresh";
-import { Dried } from "./dried";
-import { Processed } from "./processed";
-import { Contact } from "./contact";
-import { Location } from "./location";
-import { Assistence } from "./assistence";
-import { Agrokomakchi } from "./agrokomakchi";
-import { Employees } from "./employees";
 
-export {
-  Navbar,
-  Footer,
-  Hero,
-  Statistics,
-  About,
-  Brands,
-  Fresh,
-  Dried,
-  Processed,
-  Contact,
-  Location,
-  Assistence,
-  Agrokomakchi,
-  Employees,
-};
+export { Navbar, Footer, Hero };
